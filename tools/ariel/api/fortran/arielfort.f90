1fc4060832b200649668547914d074ce67909c16
Gwen <grvosku@sandia.gov>
2016-06-01 12:32:18 -0600

    ariel api: add malloc flag

--- tools/ariel/api/fortran/arielfort.f90
+++ tools/ariel/api/fortran/arielfort.f90
@@ -9,12 +9,22 @@
 MODULE arielfort
   IMPLICIT NONE
   PRIVATE
-  PUBLIC :: ariel_enable, ariel_output_stats
+  PUBLIC :: ariel_enable, ariel_output_stats, ariel_malloc_flag
   CONTAINS
+  
   SUBROUTINE ariel_enable
     WRITE(*,*) 'ARIEL: enabling simulation control'
   END SUBROUTINE ariel_enable
+  
   SUBROUTINE ariel_output_stats
 ! WRITE(*,*) 'ARIEL: printing statistics'
   END SUBROUTINE ariel_output_stats
+  
+  SUBROUTINE ariel_malloc_flag (id, mCount)
+    INTEGER(8), INTENT(IN) :: id 
+    
+    INTEGER(4), INTENT(IN) :: mCount
+    WRITE(*,*) 'ARIEL: malloc flag'
+  END SUBROUTINE
+
 END MODULE arielfort
